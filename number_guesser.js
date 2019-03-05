1e56053fb46ef6b7d95265c89a8e9bbfa27591f5
Ben Lee <bendelonlee@gmail.com>
2019-03-05 16:25:00 -0700

    Sets up game after setting min and max. Still has quirks

--- number_guesser.js
+++ number_guesser.js
@@ -1,36 +1,37 @@
+var answer;
+var min;
+var max;
+
 $( document ).ready(function() {
   let numGuesses = 0;
 
-  var min = 0;
-  var max = 0;
 
   $('.set-button').click(function() {
-    let min = parseInt($('#min-input').val());
-    let max = parseInt($('#max-input').val());
+    debugger;
+    min = parseInt($('#min-input').val());
+    max = parseInt($('#max-input').val());
     if (Number.isNaN(min) && Number.isNaN(max)) {
       $('#errors').html(`Invalid min and max. `)
     } else if (Number.isNaN(max)) {
       $('#errors').html(`Invalid maximum. `)
     } else if (Number.isNaN(min)) {
       $('#errors').html(`Invalid minimum. `)
+    } else if (min >= max) {
+      $(`#errors`).html(`Min must be less than max.`)
     } else {
-      setUpRound(min, max);
+      $(`#errors`).html('');
+      setUpRound();
     }
-  });
-
-  $('.guess-button').click(function() {
-    let guessInt = parseInt($('#number-input').val());
-    if (checkIntForErrors(guessInt, min, max)) { return true; }
-    numGuesses += 1;
-    checkGuess(guessInt, answer, numGuesses)
   });
 
   $('#clear-button').click( function() {
     $('.number-input').val('');
+    $(this).removeClass('enabled-button');
+    $('.set-button').removeClass('enabled-button');
   });
 
   $('.number-input').keyup( function(key) {
-    if($(this).val()) {
+    if($('.number-input').val()) {
       $('#enter-button').addClass('enabled-button');
       $('#clear-button').addClass('enabled-button');
     } else {
@@ -43,44 +44,70 @@ $( document ).ready(function() {
   });
 
   $('#reset-button').click( function() {
-    $('#number-input').val('');
+    $('#guess-input').val('');
     $('#feedback').val('');
+    $('#last-guess').html('&nbsp;')
     $('button').removeClass('enabled-button');
     numGuesses = 0
-    min -= 10;
+    min = min - 10;
     max += 10;
-    answer = setUpAnswer(min, max)
+    answer = setUpAnswer()
   });
+
+  function listenForGuess() {
+    $('.guess-button').click(function() {
+      let guessInt = parseInt($('#guess-input').val());
+      if (checkIntForErrors(guessInt)) { return true; }
+      numGuesses += 1;
+      checkGuess(guessInt, answer, numGuesses)
+    });
+  }
+
+  function setUpRound() {
+    $('#min-input').hide();
+    $('#max-input').hide();
+    $('#guess-input').show();
+    $('#enter-button').html('Guess')
+    $('.set-button').unbind('click');
+    $('#enter-button').removeClass('set-button')
+    $('#enter-button').addClass('guess-button')
+    setUpAnswer();
+    listenForGuess();
+  }
+
+  function setUpAnswer() {
+    $('#guess-input').attr('placeholder', `Enter a guess from ${min} to ${max}`)
+    $('#guess-input').attr('min', min)
+    $('#guess-input').attr('max', max)
+    $('#top-feedback').html('&nbsp')
+    answer = Math.floor((Math.random() * (max - min)) + min);
+  }
+
+  function checkIntForErrors(guessInt) {
+    if (Number.isNaN(guessInt)) {
+      $('#errors').html(`Invalid input. Please enter a number between ${min} and ${max}`)
+      return true;
+    } else if (guessInt > max || guessInt < min) {
+      $('#errors').html(`Out of range. The number is between ${min} and ${max}`)
+      return true;
+    } else {
+      $('#errors').html('')
+      return false;
+    }
+  }
+
+  function checkGuess(guessInt, answer, numGuesses) {
+    $('#top-feedback').html('Your last guess was');
+    $('#last-guess').html(guessInt);
+    if (guessInt > answer) {
+      $('#feedback').html('That guess is too high!')
+    } else if (guessInt < answer) {
+      $('#feedback').html('That guess is too low!')
+    } else if (guessInt == answer){
+      $('#enter-button').removeClass('enabled-button')
+      $('#feedback').html(`BOOM! You got the answer in ${numGuesses} guesses!`)
+      $('#reset-button').addClass('enabled-button')
+    }
+  }
+
 });
-
-function setUpAnswer(min, max) {
-  $('#number-input').attr('placeholder', `Enter a guess from ${min} to ${max}`)
-  $('#number-input').attr('min', min)
-  $('#number-input').attr('max', max)
-  return Math.floor((Math.random() * (max - min)) + min);
-}
-
-function checkIntForErrors(guessInt, min, max) {
-  if (Number.isNaN(guessInt)) {
-    $('#errors').html(`Invalid input. Please enter a number between ${min} and ${max}`)
-    return true;
-  } else if (guessInt > max || guessInt < min) {
-    $('#errors').html(`Out of range. The number is between ${min} and ${max}`)
-    return true;
-  } else {
-    $('#errors').html('')
-    return false;
-  }
-}
-
-function checkGuess(guessInt, answer, numGuesses) {
-  $('#last-guess').html(guessInt);
-  if (guessInt > answer) {
-    $('#feedback').html('That guess is too high!')
-  } else if (guessInt < answer) {
-    $('#feedback').html('That guess is too low!')
-  } else if (guessInt == answer){
-    $('#feedback').html(`BOOM! You got the answer in ${numGuesses} guesses!`)
-    $('#reset-button').addClass('enabled-button')
-  }
-}
